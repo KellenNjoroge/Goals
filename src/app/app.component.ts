9d4c9e00f71f0b309dfe96904d47d9f3758650ec
KellenNjoroge <muthonkel@gmail.com>
2018-08-06 17:49:15 +0300

    created goals and goal detail components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
+import { Goal } from './goal';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-    goals: string[];
-
-  constructor(){
-
-    this.goals = ["Watch Finding Nemo","Buy Cookies","Get new phone case"];
-  }
+    /*goals= [
+      new Goal(1,'Watch Wonder woman'),
+      new Goal(2,'Buy Mishiri'),
+      new Goal(3,'Get Paisley Dress'),
+      new Goal(4,'Look for Sticker'),
+      new Goal(5,'Buy Omena for Cat'),
+      new Goal(6,'Plan for my birthday party'),
+      */
+    ]
 }
---
+++ src/app/goal/goal.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {Goal} from '../goal'
+
+@Component({
+  selector: 'app-goal',
+  templateUrl: './goal.component.html',
+  styleUrls: ['./goal.component.css']
+})
+export class GoalComponent implements OnInit {
+
+  goals = [
+    new Goal(1,'Watch Wonder woman','ask Ken why he wears so many rings'),
+    new Goal(2,'Buy Mishiri','cook mishiri'),
+    new Goal(3,'Get Paisley Dress','rock it'),
+    new Goal(4,'Look for Sticker','put on the laptop'),
+    new Goal(5,'Buy Omena for Cat','hug my cat'),
+    new Goal(6,'Plan for my birthday party','invite people'),
+  ]
+
+  constructor() { }
+  ngOnInit() {
+  }
+
+}
