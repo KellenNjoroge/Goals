fa07fad39e92934271acde676cce9ad3f39b48c3
KellenNjoroge <muthonkel@gmail.com>
2018-08-07 09:19:20 +0300

    changed the two headings things

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,14 @@ import { Goal } from './goal';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-    /*goals= [
-      new Goal(1,'Watch Wonder woman'),
+    goals= [
+      /*new Goal(1,'Watch Wonder woman'),
       new Goal(2,'Buy Mishiri'),
       new Goal(3,'Get Paisley Dress'),
       new Goal(4,'Look for Sticker'),
       new Goal(5,'Buy Omena for Cat'),
-      new Goal(6,'Plan for my birthday party'),
+      new Goal(6,'Plan for my birthday party'),*/
     
-    ] */
+    ] 
+  
 }
