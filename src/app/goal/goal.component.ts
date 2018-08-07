1930690ad6d4f6ee4bb8ab162bfe320d9860e927
KellenNjoroge <muthonkel@gmail.com>
2018-08-07 15:00:50 +0300

    date in uppercase pipes

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -9,12 +9,12 @@ import {Goal} from '../goal'
 export class GoalComponent implements OnInit {
 
   goals = [
-    new Goal(1,'Watch Wonder woman','ask Ken why he wears so            many rings'),
-    new Goal(2,'Buy Mishiri','cook mishiri'),
-    new Goal(3,'Get Paisley Dress','rock it'),
-    new Goal(4,'Look for Sticker','put on the laptop'),
-    new Goal(5,'Buy Omena for Cat','hug my cat'),
-    new Goal(6,'Plan for my birthday party','invite people'),
+    new Goal(1,'Watch Wonder woman','ask Ken why he wears so many rings',new Date(2018,7,7)),
+    new Goal(2,'Buy Mishiri','cook mishiri',new Date(2018,7,7)),
+    new Goal(3,'Get Paisley Dress','rock it',new Date(2018,7,7)),
+    new Goal(4,'Look for Sticker','put on the laptop',new Date(2018,7,7)),
+    new Goal(5,'Buy Omena for Cat','hug my cat',new Date(2018,7,7)),
+    new Goal(6,'Plan for my birthday party','invite people',new Date(2018,7,7)),
   ]
   
 completeGoal(isComplete,index){
