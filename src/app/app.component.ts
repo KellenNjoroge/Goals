e5c1d891ceb7eed097bb68a0496743ed40c472ca
KellenNjoroge <muthonkel@gmail.com>
2018-08-08 09:49:50 +0300

    punctuations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,12 @@ import { Goal } from './goal';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-    goals= [
+    goals = [
       /*new Goal(1,'Watch Wonder woman'),
       new Goal(2,'Buy Mishiri'),
       new Goal(3,'Get Paisley Dress'),
       new Goal(4,'Look for Sticker'),
       new Goal(5,'Buy Omena for Cat'),
       new Goal(6,'Plan for my birthday party'),*/
-    
-    ] 
-  
+  ]
 }
